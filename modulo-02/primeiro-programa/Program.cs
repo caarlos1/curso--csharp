82d02bd40f920d4d5abf44e9f3aa74e473e3a765
Carlos Roberto <crpu4a@gmail.com>
2021-07-06 22:37:54 -0300

    m2: primeiro programa do curso.

---
+++ modulo-02/primeiro-programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carlinhosConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Olá mundo! Eu sou o primeiro programa em C# e .NET do carlinhos...");
+    }
+  }
+}
